c4b06a4e7290ce2b02858cf4d4af535b3e77542e
Hemanthuppal <131530809+Hemanthuppal@users.noreply.github.com>
2025-02-17 08:28:01 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,10 +17,7 @@ const register = async (req, res) => {
   
     try {
 
-      const existingUser = await employeeModel.getEmployeeByEmail(email);
-      if (existingUser) {
-          return res.status(400).json({ message: 'User already exists.' });
-      }
+      
 
       // Hash password
       const hashedPassword = await bcrypt.hash(password, 10);
